3cf95bdf8f0c208498399dab542bdb6e26987944
Julia Graves <jgraves@wesleyan.edu>
2017-09-14 11:24:07 -0400

    start artists component

---
+++ client/src/components/Artists.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Artists extends Component {
+	constructor() {
+		super();
+		this.state = {
+			artists: [
+				'pablo-picasso',
+				'michelangel-buonarroti',
+				'leonardo-da-vinci',
+				'salvador-dali',
+				'vincent-van-gogh',
+				'henri-matisse',
+				'georgia-okeeffe',
+				'frida-kahlo'
+			]
+		}
+	}
+
+	render() {
+		
+	}
+}
+
