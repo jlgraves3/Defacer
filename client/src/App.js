01008205db30adc6953ce702df2ef0bd31089ef1
Julia Graves <jgraves@wesleyan.edu>
2017-09-14 11:44:29 -0400

    add methods to artists component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import Nav from './components/Nav'
+import Nav from './components/Nav';
+import Artists from './components/Artists';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      painting: null,
+    }
+  }
+
   render() {
     return (
       <div className="App">
       <Nav />
+      <Artists />
       </div>
     );
   }
--- client/src/components/Artists.jsx
+++ client/src/components/Artists.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Artists extends Component {
 	constructor() {
@@ -13,12 +14,46 @@ class Artists extends Component {
 				'henri-matisse',
 				'georgia-okeeffe',
 				'frida-kahlo'
-			]
+			],
+			artistData: [],
+			artistDataLoaded: false,
 		}
+		this.renderArtist = this.renderArtist.bind(this);
+	}
+
+	componentWillMount() {
+		console.log('will mount');
+		const artistData = [];
+		this.state.artists.forEach(artist => {
+			axios.get(`/artists/${artist}`)
+			.then(res => {
+				artistData.push(res.data.data);
+				this.setState({
+						artistData: artistData,
+						artistDataLoaded: true,
+				}); 
+			})
+			.catch(err => console.log(err));
+		})
+	}
+
+	renderArtist(artist) {
+		return(
+			<div key={artist.id}>
+				<img className='thumbnail' src={artist._links.thumbnail.href} />
+				<h3>{artist.name}</h3>
+			</div>
+		)
 	}
 
 	render() {
-		
+		return (
+			<div>
+			{this.state.artistDataLoaded ? 
+				this.state.artistData.map(this.renderArtist) : ''}
+			</div>
+		)
 	}
 }
 
+export default Artists;
