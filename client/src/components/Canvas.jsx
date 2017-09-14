7b4dab2c671a3374a96147ad7103841251a64d4d
Julia Graves <jgraves@wesleyan.edu>
2017-09-14 15:52:34 -0400

    imported sketchpad into canvas

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
+//import Sketchpad from 'sketchpad'
+import {
+	SketchPad, 
+	TOOL_PENCIL, 
+	TOOL_LINE, 
+	TOOL_RECTANGLE, 
+	TOOL_ELLIPSE
+} from 'react-sketchpad/lib';
+
 
 class Canvas extends Component {
 	constructor() {
 		super();
 		this.state = {
 			height: 0,
-			width: 0, 
+			width: 0,
+			dimensionsLoaded: false, 
 		}
 		this.getDimensions = this.getDimensions.bind(this);
 	}
 	/* used https://stackoverflow.com/questions/39092859/get-dimensions-of-image-with-react
 	 thread answer to help */
 	getDimensions({target:img}) {
+		console.log('get dimensions')
 		this.setState({
 			height: img.offsetHeight,
 			width: img.offsetWidth,
+			dimensionsLoaded: true,
 		});
 	}	
 
@@ -22,13 +34,17 @@ class Canvas extends Component {
 		const src= this.props.artwork._links.image.href.replace("{image_version}","large");
 		//const {width} = this.state.width;
 		//const {height} = this.state.height; 
+		//<canvas id='sketchpad' width={this.state.width} height={this.state.height} />
 
 		return(
 			<div>
 				<h2>{this.props.artwork.title}</h2>
-				<div id='sketch-container' >
+				<div id='sketch-container' width={this.state.width} height={this.state.height}>
 					<img onLoad={this.getDimensions} src={src} />
-					<canvas width={this.state.width} height={this.state.height} />
+					{this.state.dimensionsLoaded ? 
+						<SketchPad width={this.state.width} height={this.state.height} />
+						: ''}
+					
 				</div>
 				<div id='tools'>
 					<button onClick={this.props.discard}>Discard</button>
