d8578acfd8c5b5aa7c8d2bf700cf39f3c999da16
Julia Graves <jgraves@wesleyan.edu>
2017-09-14 11:47:58 -0400

    import router in app.js, add select painting method

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Artists from './components/Artists';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      painting: null,
+      currentPainting: null,
+      selected: false,
     }
+    this.selectPainting = this.selectPainting.bind(this);
+  }
+
+  selectPainting(paiting) {
+    this.setState({
+      currentPainting: paiting,
+      selected: true,
+    })
   }
 
   render() {
