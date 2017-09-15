1a41b999d9133d768dc557bafa01c09558c6da5e
Julia Graves <jgraves@wesleyan.edu>
2017-09-15 09:41:13 -0400

    create user model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const db = require('../db/config');
+
+const User = {};
+
+User.findByUserName = username => {
+	return db.oneOrNone(`
+		SELECT * FROM users
+		WHERE username = $1
+	`,[username]);
+}
+
+User.create = user => {
+	return db.one(`
+		INSERT INTO users
+		(username,password_digest)
+		VALUES ($1,$2)
+		RETURNING *
+	`,[user.username, user.password_digest]);
+}
+
+module.exports = User;
