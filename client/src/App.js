9b5d01f18fed295e8c0c11f3725bd451b8fef240
Julia Graves <jgraves@wesleyan.edu>
2017-09-15 11:45:55 -0400

    add routes for login and register

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Nav from './components/Nav';
 import Artists from './components/Artists';
 import Works from './components/Works';
 import Canvas from './components/Canvas';
+import Register from './components/Register';
+import Login from './components/Login';
 
 import {
   BrowserRouter as Router,
@@ -55,6 +57,8 @@ class App extends Component {
               discard={this.discard} /> 
         : <Artists 
               selectArtwork={this.selectArtwork} />}
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
         </div>
       </Router>
     );
