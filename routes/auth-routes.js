acdd5fff4d50f7c0e0fb0b88d7a389a500162e4b
Julia Graves <jgraves@wesleyan.edu>
2017-09-15 09:59:31 -0400

    start auth-routes

---
+++ routes/auth-routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const authRouter = express.Router();
+const passport = require('../services/auth/local');
+const authHelpers = require('../services/auth/auth-helpers');
+const usersController = require('../controllers/users-controller');
+
+
+authRouter.post('/register', usersController.create);
