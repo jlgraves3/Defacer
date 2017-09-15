6b3a8463f7c0dd57ed2c72218c3e66b569461a4a
Julia Graves <jgraves@wesleyan.edu>
2017-09-15 10:46:17 -0400

    add links to nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Nav extends Component {
 	render () {
 		return (
 			<nav>
+				<Link to='/login'>Login</Link>
+				<Link to='/register'>Register</Link>
 			</nav>
 		);
 	}
