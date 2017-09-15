d7b17cd055111a8d2af42f5adfd5d705da02b21b
Julia Graves <jgraves@wesleyan.edu>
2017-09-15 10:27:35 -0400

    add create method to users controller

---
+++ controllers/users-controller.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+
+const usersController = {};
+
+//create new user
+usersController.create = (req,res) => {
+	//encrypt password
+	const salt = bcrypt.genSaltSync();
+	const hash = bcrypt.hashSync(req.body.password, salt);
+	//add to database
+	User.create({
+		username: req.body.username,
+		password_digest: hash
+	}).then(user => {
+		req.login(user, (err) => {
+			if (err) return next(err);
+		});
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({error:err});
+	});
+}
+
+module.exports = usersController; 
