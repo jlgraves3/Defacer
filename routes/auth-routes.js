67a00a82041a14ea2b9fd615269e8e6b40eb7c43
Julia Graves <jgraves@wesleyan.edu>
2017-09-15 10:31:41 -0400

    add routes to auth-routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -4,5 +4,18 @@ const passport = require('../services/auth/local');
 const authHelpers = require('../services/auth/auth-helpers');
 const usersController = require('../controllers/users-controller');
 
-
+//register - create new user
 authRouter.post('/register', usersController.create);
+
+//login
+authRouter.post('/login', passport.authenticate('local'));
+
+//logout
+authRouter.get('/logout', (req,res) => {
+	req.logout();
+	res.json({
+		message: 'logout successful',
+	});
+});
+
+module.exports = authRouter;
