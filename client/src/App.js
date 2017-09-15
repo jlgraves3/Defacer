18ff79e11c4d3011b602158144b7a3783ada7cf8
Julia Graves <jgraves@wesleyan.edu>
2017-09-15 14:57:48 -0400

    create auth handler methods

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
+
 import Nav from './components/Nav';
-import Artists from './components/Artists';
-import Works from './components/Works';
-import Canvas from './components/Canvas';
 import Register from './components/Register';
 import Login from './components/Login';
 import Home from './components/Home';
+
 
 import {
   BrowserRouter as Router,
@@ -15,6 +15,31 @@ import {
 } from 'react-router-dom';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    }
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleRegister = this.handleRegister.bind(this);
+    this.handleLogOut = this.handleLogOut.bind(this);
+  }
+
+  handleLogin(data) {
+    alert(data);
+  }
+
+  handleRegister(data) {
+    alert(data)
+  }
+
+  handleLogOut() {
+    axios.get('/auth/logout')
+    .then(res => {
+      alert(res.data);
+    })
+  }
+
   render() {
     return (
       <Router>
