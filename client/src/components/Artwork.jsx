6da6ec0dccf0af7b08c877cd34ad8fb5f06a5553
Julia Graves <jgraves@wesleyan.edu>
2017-09-18 16:15:09 -0400

    render single artworks

--- client/src/components/Artwork.jsx
+++ client/src/components/Artwork.jsx
@@ -1,13 +1,42 @@
-const Artwork = props => {
-	return(
-		<div>
-			<h1>{props.artwork.name || 'Untitled'}</h1>
-			<div className='artwork-single'>
-				<img src={props.artwork.painting_src} />
-				<img src={props.artwork.canvas_src} />
-			</div>
-		</div>
-	)
-} 
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Artwork extends Component {
+	constructor() {
+		super();
+		this.state = {
+			artwork: null,
+			artworkLoaded: false,
+		}
+	}
+
+	componentWillMount() {
+		console.log(this.props)
+		axios.get(`/gallery/${this.props.match.params.id}`)
+		.then(res => {
+			this.setState({
+				artwork: res.data.data,
+				artworkLoaded: true,
+			});
+		}).catch(err => console.log(err));
+	}
+
+	render() {
+		if (this.state.artworkLoaded) {
+			const artwork = this.state.artwork;
+			return (
+				<div className='single'>
+					<h1>{artwork.title || 'Untitled'}</h1>
+					<div className='artwork-single'>
+						<img src={artwork.painting_src} />
+						<img src={artwork.canvas_src} />
+					</div>
+				</div>
+			)
+		} else {
+			return <div></div>
+		}
+	}
+}
 
 export default Artwork;
