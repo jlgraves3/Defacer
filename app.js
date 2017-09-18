ce24d1558a6ab0a7f63e7ba3d68f2a6e8b97f260
Julia Graves <jgraves@wesleyan.edu>
2017-09-18 11:08:57 -0400

    add auth routes and auth middlewares to app.js

--- app.js
+++ app.js
@@ -3,33 +3,31 @@ const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 //auth dependencies
-/*const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const passport = require('passport');*/
-
-const jwt = require('express-jwt');
-const jwks = require('jwks-rsa');
-
+const passport = require('passport');
+comnst methodOverride = require('method-override');
 
 const app = express();
 
 require('dotenv').config();
 
+//middlewares
 app.use(logger('dev'));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-//authentication middlewares
-/*app.use(cookieParser());
+//auth middlewares
+app.use(methodOverride('_method'));
+app.use(cookieParser());
 app.use(session({
 	secret: process.env.SECRET_KEY,
 	resave: false,
 	saveUninitialized: true,
 }));
 app.use(passport.initialize());
-app.use(passport.session()); 
-*/
+app.use(passport.session());
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
@@ -42,34 +40,11 @@ app.get('/', (req,res) => {
 });
 
 
-// from tutorial: https://auth0.com/blog/reactjs-authentication-tutorial/
-const authCheck = jwt({
-	secret: jwks.expressJwtSecret({
-		cache: true,
-		rateLimit: true,
-		jwksRequestsPerMinute: 5,
-		jwksUri: "https://vandalizer.auth0.com/.well-known/jwks.json"
-	}),
-	audience: 'http://vandelizer-auth.com',
-	issuer: "https://vandalizer.auth0.com/",
-	algorithms: ['RS256']
-});
-
-app.use(authCheck);
-
-app.get('/authorized', (req,res) => {
-	res.send('Secured Resource');
-});
-
 const artistRoutes = require('./routes/artist-routes');
 app.use('/artists', artistRoutes);
 
-//const authRoutes = require('./routes/auth-routes');
-//app.use('/auth',authRoutes);
-
-const userRoutes = require('./routes/user-routes');
-app.use('/user', userRoutes);
-
+const authRoutes = require('./routes/auth-routes');
+app.use('/auth', authRoutes);
 
 //error handler
 app.use('*', (req,res) => {
