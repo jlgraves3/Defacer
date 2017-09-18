c0fb6f58527eaa097b9702eddde3ccad60298c09
Julia Graves <jgraves@wesleyan.edu>
2017-09-18 18:38:13 -0400

    added methods to profile

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class Profile extends Component {
+	constructor() {
+		super();
+		this.state = {
+			userArtworks: null,
+			userArtworksLoaded: false,
+		}
+		this.renderArtwork = this.renderArtwork.bind(this);
+	}
+
+	componentWillMount() {
+		if (this.props.loggedIn) {
+			//get artworks by logged in user
+			axios.get(`/gallery/user/${this.props.user.id}`)
+			.then(res => {
+				this.setState({
+					userArtworks: res.data.data,
+					userArtworksLoaded: true,
+				});
+			}).catch(err => console.log(err));
+		}
+	}
+
+	renderArtwork(artwork) {
+		return(
+			<Link to={`/gallery/${artwork.id}`} key={artwork.id}>
+			<div className='gallery-work'>
+				<div className='overlay'>
+					<h4>{artwork.title || 'Untitled'}</h4>
+				</div>
+				<img src={artwork.painting_src} />
+				<img src={artwork.canvas_src} />	
+			</div>
+			</Link>
+		)
+	}
+
+	render() {
+		if (!this.props.loggedIn) {
+			return <Redirect to='/login' />
+		}
+		return(
+			<div>
+				<h1>{this.props.user.username}</h1>
+				<h2>Your Artworks:</h2>
+				{this.state.userArtworksLoaded ? this.state.userArtworks.map(this.renderArtwork) : ''}
+
+			</div>
+		)
+	}
+}
+
+export default Profile;
