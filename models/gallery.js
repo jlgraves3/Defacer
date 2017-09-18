9a0ae0a0e79cfab429f02cee51d61c974d79b98f
Julia Graves <jgraves@wesleyan.edu>
2017-09-18 10:46:01 -0400

    add delete and find methods

--- models/gallery.js
+++ models/gallery.js
@@ -2,12 +2,30 @@ const db = require('../db/config');
 
 const Gallery = {};
 
+Gallery.findAll = () => {
+	return db.query("SELECT * FROM gallery");
+}
+
+Gallery.findById = id => {
+	return db.oneOrNone(`
+		SELECT * FROM gallery 
+		WHERE id = $1
+	`,[id]);
+}
+
 Gallery.create = artwork => {
 	db.one(`
 		INSERT INTO gallery	(user_id,title,painting_src,canvas_src)
 		VALUES ($1,$2,$3,$4)
 		RETURNING *
 	`,[artwork.user_id,artwork.title,artwork.painting_src,artwork.canvas_src]);
+}
+
+Gallery.delete = id => {
+	return db.none(`
+		DELETE FROM gallery
+		WHERE id = $1
+	`,[id])
 }
 
 module.exports = Gallery;
