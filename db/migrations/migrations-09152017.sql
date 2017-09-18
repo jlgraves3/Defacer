0dd9b48d03147363bb97fba3ce1af62a26023c14
Julia Graves <jgraves@wesleyan.edu>
2017-09-18 10:35:22 -0400

    add gallery table to mirgation

--- db/migrations/migrations-09152017.sql
+++ db/migrations/migrations-09152017.sql
@@ -5,3 +5,11 @@ CREATE TABLE IF NOT EXISTS users (
 	username VARCHAR(255) UNIQUE NOT NULL,
 	password_digest TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS gallery (
+	id SERIAL PRIMARY KEY,
+	user_id REFERENCES users(id),
+	title VARCHAR(255),
+	painting_src VARCHAR(255),
+	canvas_src VARCHAR(255),
+);
