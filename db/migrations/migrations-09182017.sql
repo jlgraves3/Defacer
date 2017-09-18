05e2deaff41677131ac74bd97038b2a926017b6f
Julia Graves <jgraves@wesleyan.edu>
2017-09-18 10:37:58 -0400

    fix type

--- db/migrations/migrations-09182017.sql
+++ db/migrations/migrations-09182017.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS gallery (
 	id SERIAL PRIMARY KEY,
-	user_id REFERENCES users(id),
+	user_id INTEGER REFERENCES users(id),
 	title VARCHAR(255),
 	painting_src VARCHAR(255),
 	canvas_src VARCHAR(255)
