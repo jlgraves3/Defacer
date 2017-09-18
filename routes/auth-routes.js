c03f2cdb632b4ed2c20750b9db054a7c4b643606
Julia Graves <jgraves@wesleyan.edu>
2017-09-18 11:03:34 -0400

    create auth-routes delete user-routes

---
+++ routes/auth-routes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const authRouter = express.Router();
+const passport = require('../services/auth/local');
+const authHelpers = require('../services/auth/auth-helpers');
+const usersController = require('../controllers/users-controller');
+
+//register new user
+authRouter.post('/register', usersController.create);
+
+//authenticate user
+authRouter.post('/login', passport.authenticate('local', {
+	successRedirect: '/auth/success',
+	failureRedirect: '/login',
+	failureFlash: true,
+}));
+
+//user is authenticated
+authRouter.get('/success',(req,res) => {
+	res.json({
+		auth: true,
+		message: 'ok',
+		user: req.user,
+	});
+});
+
+//user is not authenticated
+authRouter.get('/failure',(req,res) => {
+	res.json({
+		auth: false,
+		message: 'login failed',
+		user: null,
+	});
+});
+
+
+authRouter.get('/logout', (req,res) => {
+	req.logout();
+	res.redirect('/');
+});
+
+module.exports = authRouter;
--- routes/user-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userRoutes = express.Router();
-const usersController = require('../controllers/users-controller');
-const authHelpers = require('../services/auth/auth-helpers');
-
-userRoutes.get('/', usersController.index);
-
-module.exports = userRoutes;
