6e1dd1d74a388382166397acfef76cd7818243f8
Julia Graves <jgraves@wesleyan.edu>
2017-09-18 12:29:02 -0400

    change varchar to text

--- db/migrations/migrations-09152017.sql
+++ db/migrations/migrations-09152017.sql
@@ -6,10 +6,3 @@ CREATE TABLE IF NOT EXISTS users (
 	password_digest TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS gallery (
-	id SERIAL PRIMARY KEY,
-	user_id REFERENCES users(id),
-	title VARCHAR(255),
-	painting_src VARCHAR(255),
-	canvas_src VARCHAR(255),
-);
--- db/migrations/migrations-09182017.sql
+++ db/migrations/migrations-09182017.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS gallery (
 	id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users(id),
 	title VARCHAR(255),
-	painting_src VARCHAR(255),
-	canvas_src VARCHAR(255)
+	painting_src TEXT,
+	canvas_src TEXT
 );
