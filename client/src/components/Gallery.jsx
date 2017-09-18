0938ac39cd80a8d80b5f36491de44a12214b6788
Julia Graves <jgraves@wesleyan.edu>
2017-09-18 13:49:24 -0400

    render galler items

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Gallery extends Component {
-	
+	constructor() {
+		super();
+		this.state = {
+			artworks: null,
+			artworksLoaded: false,
+		}
+		this.renderArtwork = this.renderArtwork.bind(this);
+	}
+
+	componentWillMount() {
+		axios.get('/gallery')
+		.then(res => {
+			this.setState({
+				artworks: res.data.data,
+				artworksLoaded: true,
+			});
+		}).catch(err => console.log(err));
+	}
+
+	renderArtwork(artwork) {
+		return(
+			<div className='gallery-work'>
+				<img className='bottom' src={artwork.painting_src} />
+				<img className='top' src={artwork.canvas_src} />
+			</div>
+		)
+	}
+
 	render() {
 		return(
 			<div>
 				<h1>Gallery</h1>
+				<div className='gallery-container'>
+				{this.state.artworksLoaded ? this.state.artworks.map(this.renderArtwork) : ''}
+				</div>
 			</div>
 		)
 	}
