2624bd848d8b8bb76bd3b39f8cc072ba50667e83
Julia Graves <jgraves@wesleyan.edu>
2017-09-19 10:20:30 -0400

    fix syntax error

--- db/config.js
+++ db/config.js
@@ -1,13 +1,15 @@
 const pgp = require('pg-promise')();
 
 let db;
+
 if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
 	db = pgp({
 		database: 'vandalizer_dev',
 		port: 5432,
 		host: 'localhost',
 	});
-} else if (process.env.NODE_ENV == 'production'); 
+} else if (process.env.NODE_ENV == 'production') {
 	db = pgp(process.env.DATABASE_URL);
 }
+
 module.exports = db;
