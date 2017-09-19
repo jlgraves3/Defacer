dcbf4ea5a1bf3995958ddfe9ba8a6dbc2bc21f55
Julia Graves <jgraves@wesleyan.edu>
2017-09-19 10:18:47 -0400

    fix config.js

--- db/config.js
+++ db/config.js
@@ -1,15 +1,13 @@
-const pgp = require('pg-promise')(options);
+const pgp = require('pg-promise')();
 
 let db;
-
-if (process.env.NODE_ENV === 'development' || !process.env.development) {
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
 	db = pgp({
 		database: 'vandalizer_dev',
 		port: 5432,
 		host: 'localhost',
-	})
+	});
 } else if (process.env.NODE_ENV == 'production'); 
 	db = pgp(process.env.DATABASE_URL);
 }
-
 module.exports = db;
