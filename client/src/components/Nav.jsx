b9aca21ffa96f953e172925fa81d5f3d02942cf4
Julia Graves <jgraves@wesleyan.edu>
2017-09-20 11:39:17 -0400

    remove about link

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -8,7 +8,6 @@ class Nav extends Component {
 				<div>
 				<Link to='/' id='logo'></Link>	
 				<Link to='/gallery'>Gallery</Link>
-				<a>About</a>
 				</div>
 				<div>	
 				{this.props.loggedIn ? <Link to='/profile'><i className="fa fa-user" aria-hidden="true"></i> {this.props.user.username}</Link> : <Link to='/login'>Log In</Link>}	
