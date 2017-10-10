4988293c3aa9f41695651913a286d6e3f8263515
Julia Graves <jgraves@wesleyan.edu>
2017-10-10 12:50:50 -0400

    add create and delete methods

--- models/favorites.js
+++ models/favorites.js
@@ -1,9 +1,9 @@
 const db = require('../db/config');
-const Favorites = {};
+const Favorite = {};
 
 
 //finds all artworks user has favorited
-Favorites.findByUser = user_id => {
+Favorite.findByUser = user_id => {
 	return db.query(`
 		SELECT * FROM 
 		gallery JOIN favorites ON
@@ -11,3 +11,22 @@ Favorites.findByUser = user_id => {
 		WHERE favorites.user_id = $1
 		`, [user_id]);
 }
+
+Favorite.create = (gallery_id,user_id) {
+	return db.one(`
+		INSERT INTO favorites (gallery_id, user_id)
+		VALUES ($1,$2)
+		RETURNING *
+	`,[gallery_id,user_id]);
+} 
+
+Favorite.delete = (gallery_id,user_id) {
+	return db.none(`
+		DELETE FROM favorites WHERE
+		gallery_id = $1 AND
+		user_id = $2
+	`,[gallery_id,user_id]);
+} 
+
+
+module.exports = Favorite;
