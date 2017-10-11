336228298c0abd1997107f2d5d7caf3eca1a1350
Julia Graves <jgraves@wesleyan.edu>
2017-10-11 12:13:35 -0400

    add favorite routes

--- routes/gallery-routes.js
+++ routes/gallery-routes.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const galleryRoutes = express.Router();
 
-const galleryController = require('../controllers/gallery-controller')
+const galleryController = require('../controllers/gallery-controller');
 
 galleryRoutes.get('/', galleryController.index);
 galleryRoutes.get('/:id', galleryController.show);
 galleryRoutes.get('/user/:user_id', galleryController.showUser);
 
+galleryRoutes.get('/user/:user_id/favorites', galleryController.showUserFavorites);
+
 galleryRoutes.post('/', galleryController.create);
 galleryRoutes.put('/:id',galleryController.update);
 
 galleryRoutes.delete('/:id', galleryController.destroy);
+
+galleryRoutes.post('/:id/favorite', galleryController.favorite);
+galleryRoutes.delete('/:id/favorite', galleryController.unfavorite);
 
 module.exports = galleryRoutes;
