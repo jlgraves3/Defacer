4d0f5489b63648876190e8b72ad8e57e2febfac3
Julia Graves <jgraves@wesleyan.edu>
2017-10-16 12:23:34 -0400

    add favoriting functions

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -9,34 +9,92 @@ class Gallery extends Component {
 		this.state = {
 			artworks: null,
 			artworksLoaded: false,
+			message: '',
+			favorites: null,
 		}
 		this.renderArtwork = this.renderArtwork.bind(this);
+		this.favorite = this.favorite.bind(this);
+		this.unfavorite = this.unfavorite.bind(this);
+		this.toggleFavorite = this.toggleFavorite.bind(this);
 	}
 
 	componentDidMount() {
 		//fetch artworks from gallery
 		axios.get('/gallery')
 		.then(res => {
+			const artworks = res.data.data
 			this.setState({
-				artworks: res.data.data,
+				artworks: artworks,
 				artworksLoaded: true,
 			});
+			const artworkFavorites = {};
+			//gets number of favorites for each artwork
+			artworks.forEach(artwork => artworkFavorites[artwork.id] = artwork.count);
+			//console.log(artworkFavorites);
 		}).catch(err => console.log(err));
+	}
+
+	favorite(id) {
+		if (this.props.loggedIn) {
+			axios.post(`/gallery/${id}/favorite`)
+			.then(() => {
+				//update user favorites
+				const userFavorites = this.props.userFavorites;
+				userFavorites[id] = true;
+				this.props.handleUserFavorites(userFavorites);
+				//update artwork favorites count 
+				const artworkFavorites = this.props.artworkFavorites;
+				artworkFavorites[id] = artworkFavorites[id] + 1
+				this.props.handleArtworkFavorites(artworkFavorites);
+			})
+			.catch(err => console.log(err));
+		} else {
+			this.setState({
+				message: 'You must be logged in to favorite.'
+			});
+		}
+	}
+
+	unfavorite(id) {
+		if (this.props.loggedIn) {
+			axios.delete(`/gallery/${id}/favorite`)
+			.then(() => {
+				const newFavorites = this.props.favorites;
+				newFavorites[id] = false;
+				this.props.handleUserFavorites(newFavorites);
+				console.log('unfavorited');
+			})
+			.catch(err => console.log(err));
+		}
+	}
+
+	//favorite / unfavorite
+	toggleFavorite(id) {
+		console.log(this.props);
+		if (this.props.favorites[id]) {
+			this.unfavorite(id);
+		} else {
+			this.favorite(id);
+		}
 	}
 
 	//render single artwork div
 	renderArtwork(artwork) {
 		return(
-			<Link to={`/gallery/${artwork.id}`} key={artwork.id}>
-			<div className='gallery-work'>
-				<div className='overlay'>
-					<h4>{artwork.title || 'Untitled'}</h4>
-					<h5>{artwork.username}</h5>
-				</div>
+			<div className='gallery-work' key={artwork.id}>
+				<Link to={`/gallery/${artwork.id}`} >
+					<div className='overlay'>
+						<h4>{artwork.title || 'Untitled'}</h4>
+						<h5>{artwork.username}</h5>
+					</div>
 				<img src={artwork.painting_src} alt=''/>
 				<img src={artwork.canvas_src} alt='' />	
-			</div>
-			</Link>
+				</Link>
+				<p><i className={`${this.props.userFavorites[artwork.id] ? "fa fa-heart favorited" : "fa fa-heart-o"}`} 
+					aria-hidden="true" 
+					onClick={() => this.toggleFavorite(artwork.id)}></i> 
+					{artwork.count > 0 ? " " + artwork.count : ' '}</p>︎
+			</div>	
 		)
 	}
 
